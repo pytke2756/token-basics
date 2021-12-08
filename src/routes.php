7484ddd408c8a56dcb46b8760380186421794a21
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-08 13:48:29 +0100

    C:/Program Files/Git/api route add

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Petrik\Loginapp\User;
 use Petrik\Loginapp\Token;
+use Slim\Routing\RouteCollectorProxy;
 
 return function(App $app){
     $app->get('/', function (Request $request, Response $response, $args) {
@@ -43,6 +44,14 @@ return function(App $app){
         return $response->withHeader('Content-Type', 'application/json')
                 ->withStatus(200);
     });
-
+    $app->group("/api", function(RouteCollectorProxy $group){
+        $group->get('/hello', function(Request $request, Response $response, $args){
+            $response->getBody()->write(json_encode([
+                'Hello' => 'Wolrld',
+            ]));
+            return $response->withHeader('Content-Type', 'application/json')
+                    ->withStatus(200);
+        });
+    });
 
 };
