c76305a1a84a889bc79d8b5c0d0ccb3c3962ab61
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-08 14:22:46 +0100

    AuthMiddlewware created

---
+++ src/Middlewares/AuthMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Petrik\Loginapp\Middlewares;
+
+use Exception;
+use Petrik\Loginapp\Token;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class AuthMiddleware{
+    public function __invoke(Request $request, RequestHandler $handler):Response
+    {
+        $auth = $request->getHeader('Authorization');
+        if (count($auth) !== 1) {
+            throw new Exception('Hibás Authorization header');
+        }
+        $authArr = mb_split(' ', $auth[0]);
+        if ($authArr[0] !== 'Bearer') {
+            throw new Exception("Nem támogatott autentikációs módszer");
+        }
+        $tokenStr = $authArr[1];
+        $token = Token::where('token', $tokenStr)->firstOrFail();
+
+        //user kikeresése, eltérolása
+
+        return $handler->handle($request);
+    }
+}
