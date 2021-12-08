bfb6244d3cc1a6bbe044e9258af3f84fb814a731
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-08 12:08:22 +0100

    User.php add

---
+++ src/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Petrik\Loginapp;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model{
+    //ha created_at / updated_at nélkül hoztuk létre:
+    //protected $timestamps = false;
+}
