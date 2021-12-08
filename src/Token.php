7f8e81e7937ab6315da533280d924b8bef01b544
Levente Ibrányi <ibranyilevente@gmail.com>
2021-12-08 12:08:06 +0100

    Token.php add

---
+++ src/Token.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Petrik\Loginapp;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model{
+    //ha created_at / updated_at nélkül hoztuk létre:
+    //protected $timestamps = false;
+}
